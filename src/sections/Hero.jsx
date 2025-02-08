2991e4de9b0acc5bd914404b8c926eeaa1707255
rayidwika <rayilp3i@gmail.com>
2025-02-08 21:14:44 +0700

    initial commit

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+    return (
+    <section className="bg-gradient-to-br from-slate-950 to-blue-800 text-white">
+        <div className="min-h-screen max-w-screen-lg m-auto flex items-center justify-center gap-8">
+            <div>
+                <p className="text-lg">hi, my name is...</p>
+                <h1 className="text-7xl font-bold">Rayi Dwika</h1>
+            </div>
+            <div>
+                <img src="/public/mulyono.PNG" className="border-b-2" alt="test" />    
+            </div>                                  
+        </div>
+    </section>
+    )
+}
